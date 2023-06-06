ba19c5336125a21b82c0d14df5139a1df83357f4
Kirill <scerep529@gmail.com>
2023-06-06 23:15:41 +0600

    Implement logout logic

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,21 +1,20 @@
 import { ChevronLeftOutlined, LogoutOutlined } from '@mui/icons-material';
 import {
-  Box,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-  useTheme,
+	Box,
+	Drawer,
+	IconButton,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	Typography,
+	useTheme,
 } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/images/sidebar/logo.svg';
 import { ISidebar } from '../../common/types/sidebar';
-import { tokens } from '../../theme';
 import FlexBetween from '../flex-between';
 import SearchBarComponent from '../search-bar';
 import ThemeSwitcherComponent from '../theme-switcher';
@@ -23,92 +22,100 @@ import { NavBarComponent } from './navbar';
 import { useStyles } from './styles';
 
 const SidebarComponent: FC<ISidebar> = ({
-  isNonMobile,
-  isOpen,
-  setIsOpen,
-  drawerWidth,
+	isNonMobile,
+	isOpen,
+	setIsOpen,
+	drawerWidth,
 }: ISidebar): JSX.Element => {
-  const [active, setActive] = useState<string>('');
-  const classes = useStyles();
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+	const [active, setActive] = useState<string>('');
+	const classes = useStyles();
+	const { pathname } = useLocation();
+	const navigate = useNavigate();
+	const theme = useTheme();
 
-  useEffect(() => {
-    setActive(pathname);
-  }, [pathname]);
+	useEffect(() => {
+		setActive(pathname);
+	}, [pathname]);
 
-  return (
-    <Box component="nav">
-      {isOpen && (
-        <Drawer
-          anchor="left"
-          open={isOpen}
-          onClose={() => setIsOpen(false)}
-          variant="persistent"
-          sx={{
-            width: drawerWidth,
-            '& .MuiDrawer-paper': {
-              color: theme.palette.secondary.main,
-              backgroundColor: theme.palette.primary.main,
-              boxSizing: 'border-box !important',
-              width: '250px',
-            },
-          }}
-        >
-          <Box className={classes.navBlock}>
-            <Box>
-              <FlexBetween>
-                <Box className={classes.brand} onClick={() => navigate('/')}>
-                  <img src={Logo} alt="Logo" />
-                  <Typography variant="h1" className={classes.brandTitle}>
-                    Demo
-                  </Typography>
-                </Box>
-                {!isNonMobile && (
-                  <IconButton onClick={() => setIsOpen(!isOpen)}>
-                    <ChevronLeftOutlined />
-                  </IconButton>
-                )}
-              </FlexBetween>
-            </Box>
-            <List className={classes.navList}>
-              <List>
-                {!isNonMobile && (
-                  <ListItem>
-                    <SearchBarComponent />
-                  </ListItem>
-                )}
-              </List>
-              <NavBarComponent active={active} />
-            </List>
-          </Box>
-          <Box width="100%">
-            <List>
-              {!isNonMobile && (
-                <ListItem>
-                  <Box padding="5px">
-                    <ThemeSwitcherComponent />
-                  </Box>
-                </ListItem>
-              )}
-              <ListItem>
-                <ListItemButton className={classes.navItem}>
-                  <ListItemIcon>
-                    <LogoutOutlined className={classes.icons} />
-                  </ListItemIcon>
-                  <ListItemText>
-                    <Typography>Выход</Typography>
-                  </ListItemText>
-                </ListItemButton>
-              </ListItem>
-            </List>
-          </Box>
-        </Drawer>
-      )}
-    </Box>
-  );
+	const handleLogout = () => {
+		sessionStorage.removeItem('name');
+		sessionStorage.removeItem('token');
+		navigate('/login');
+	};
+
+	return (
+		<Box component='nav'>
+			{isOpen && (
+				<Drawer
+					anchor='left'
+					open={isOpen}
+					onClose={() => setIsOpen(false)}
+					variant='persistent'
+					sx={{
+						width: drawerWidth,
+						'& .MuiDrawer-paper': {
+							color: theme.palette.secondary.main,
+							backgroundColor: theme.palette.primary.main,
+							boxSizing: 'border-box !important',
+							width: '250px',
+						},
+					}}
+				>
+					<Box className={classes.navBlock}>
+						<Box>
+							<FlexBetween>
+								<Box className={classes.brand} onClick={() => navigate('/')}>
+									<img src={Logo} alt='Logo' />
+									<Typography variant='h1' className={classes.brandTitle}>
+										Demo
+									</Typography>
+								</Box>
+								{!isNonMobile && (
+									<IconButton onClick={() => setIsOpen(!isOpen)}>
+										<ChevronLeftOutlined />
+									</IconButton>
+								)}
+							</FlexBetween>
+						</Box>
+						<List className={classes.navList}>
+							<List>
+								{!isNonMobile && (
+									<ListItem>
+										<SearchBarComponent />
+									</ListItem>
+								)}
+							</List>
+							<NavBarComponent active={active} />
+						</List>
+					</Box>
+					<Box width='100%'>
+						<List>
+							{!isNonMobile && (
+								<ListItem>
+									<Box padding='5px'>
+										<ThemeSwitcherComponent />
+									</Box>
+								</ListItem>
+							)}
+							<ListItem>
+								<ListItemButton
+									onClick={handleLogout}
+									className={classes.navItem}
+								>
+									<ListItemIcon>
+										<LogoutOutlined className={classes.icons} />
+									</ListItemIcon>
+									<ListItemText>
+										<Typography>Выход</Typography>
+									</ListItemText>
+								</ListItemButton>
+							</ListItem>
+						</List>
+					</Box>
+				</Drawer>
+			)}
+		</Box>
+	);
 };
 
 export default SidebarComponent;
--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,21 +1,45 @@
 import { Box, Grid, Link, Typography } from '@mui/material';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { getNews } from '../../store/thunks/news';
 import { useAppDispatch, useAppSelector } from '../../utils/hooks';
 import { useStyles } from './styles';
 
 const NewsPage: FC = (): JSX.Element => {
+	const [newsItem, setNewsItem] = useState([]);
+	const [items, setItems] = useState<number>(4);
 	const dispatch = useAppDispatch();
 	const { news } = useAppSelector(state => state.news);
 	const classes = useStyles();
+
+	useEffect(() => {
+		setNewsItem(news.slice(0, items));
+		console.log(items);
+	}, [news, items]);
+
+	useEffect(() => {
+		document.addEventListener('scroll', handleScroll);
+
+		return () => {
+			document.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	useEffect(() => {
 		dispatch(getNews());
 	}, []);
 
-	console.log(news);
+	const handleScroll = (e: any) => {
+		if (
+			e.target.documentElement.scrollHeight -
+				(e.target.documentElement.scrollTop + window.innerHeight) <
+			100
+		) {
+			setItems(prev => prev + 4);
+		}
+	};
 
-	const renderNewsBlock = news.map((element: any) => (
-		<Grid container className={classes.newsBlock}>
+	const renderNewsBlock = newsItem.map((element: any) => (
+		<Grid key={element.id} container className={classes.newsBlock}>
 			<Grid item xs={12} md={3} className={classes.image}>
 				<img src={element.imageurl} alt={element.categories} />
 			</Grid>
